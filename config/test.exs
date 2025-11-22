7dd313fb8c68cebdbcb3c0adaab267c2e9adedf0
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2025-11-21 15:53:52 -1000

    fb

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,23 @@
 import Config
 
+test_db_host = System.get_env("POSTGRES_HOST") || "localhost"
+test_db_port = String.to_integer(System.get_env("POSTGRES_PORT") || "5432")
+test_db_user = System.get_env("POSTGRES_USER") || "postgres"
+test_db_pass = System.get_env("POSTGRES_PASSWORD") || "postgres"
+test_db_partition = System.get_env("MIX_TEST_PARTITION") || ""
+
+test_db_name =
+  case System.get_env("POSTGRES_DB") do
+    nil -> "cns_ui_test#{test_db_partition}"
+    db -> "#{db}#{test_db_partition}"
+  end
+
 config :cns_ui, CnsUi.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "cns_ui_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: test_db_user,
+  password: test_db_pass,
+  hostname: test_db_host,
+  port: test_db_port,
+  database: test_db_name,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
