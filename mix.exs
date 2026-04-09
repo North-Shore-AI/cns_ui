86dc3852b82767a16036a1cf63295baa75bfff3f
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-04-08 19:47:26 -1000

    Refresh mix deps and package docs files

--- mix.exs
+++ mix.exs
@@ -79,7 +79,7 @@ defmodule CnsUi.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
-      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.40.0", only: :dev, runtime: false},
       {:ex_machina, "~> 2.7", only: :test},
       {:faker, "~> 0.17", only: :test},
 
